b8f93df36a5418cb505560eb8121a2a6fdbd26ae
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-23 15:05:54 +0000

    comments

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 let number = 0
 let guess = 0
-
+// variables 
 number = Math.random() * 6 + 1
 number = parseInt(number)
-
+// The random number generater, math.random makes sure its not as decimals, the little equation in the back sets the range and parseint turns it into an integer
 document.getElementById('button').addEventListener('click', random)
-
+// function once the button is pressed
 function random () {
-  // gets the values from the number in the text box
+  
   let guess = document.getElementById('input').value
   guess = parseInt(guess)
-
+// gets the value from the inputbox and turns it into a number
   if (guess === number) {
     document.getElementById('paratext').innerHTML = 'You got it Correct!'
   } else {
     document.getElementById('paratext').innerHTML = 'Incorrect! Try Again'
   }
 }
+// compares the input with the random number and uses the if...else statement to show text depending on the answer
 
 document.getElementById('reset').addEventListener('click', reset)
 
 function reset () {
   location.reload()
 }
+// resets the page by refreshing it so it can come up with a new number
